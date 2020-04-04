d6a9917c742878167110e9ca8a573d40fdd12242
Florian Gardin <fgardin@quantmetry.com>
2020-04-04 17:30:16 +0200

    add midi player

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
- import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MIDISounds from 'midi-sounds-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: [],
+            midi_to_play: [{pitch: [60], duration: 2.5, time:1, track: 1}],
+            instruments: {0: 3, 1:3 },
+            tempo: 120
+        };
+    }
+
+    componentDidMount() {
+
+    }
+
+    loadScoreFromBackend(){
+
+    }
+
+    playTestInstrument() {
+        // Request backend return json
+        // If the user want he can download the midi file from the backend
+        let track_context = this.state.midi_to_play[i];
+        for(var i=0; i<this.props.state; i++){
+            let to_play = this.state.midi_to_play[i];
+            this.midiSounds.playChordAt(to_play.time, this.state.instruments[to_play.track],
+                to_play.pitch, to_play.duration);
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to midi-sounds-react example 1</h1>
+                </header>
+                <p className="App-intro">Press Play to play instrument sound.</p>
+                <p><button onClick={this.playTestInstrument.bind(this)}>Play</button></p>
+                <MIDISounds ref={(ref) => (this.midiSounds = ref)} appElementName="root" instruments={[3]} />
+            </div>
+        );
+    }
 }
 
 export default App;
