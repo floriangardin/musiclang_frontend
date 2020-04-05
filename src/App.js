84578272a7aeffef3a1e70c3f5704d6fe97188d2
Florian Gardin <fgardin@quantmetry.com>
2020-04-05 21:22:34 +0200

    add volume in play midi

--- src/App.js
+++ src/App.js
@@ -11,9 +11,16 @@ class App extends Component {
             error: null,
             isLoaded: false,
             items: [],
-            midi_to_play: [{pitch: [60], duration: 2.5, time:1, track: 1}],
+            midi_to_play: [{pitch: 60, duration: 2.5, offset:1, track: 1, volume:100},
+                {pitch: 64, duration: 2.5, offset:2, track: 1, volume: 70},
+                {pitch: 67, duration: 2.5, offset:3, track: 1, volume: 20},
+                {pitch: 67, duration: 2.5, offset:4, track: 1, volume: 10}
+            ],
             instruments: {0: 3, 1:3 },
-            tempo: 120
+            tempo: 120,
+            currentOffset: 0,
+            startTime: 0,
+            durationQuarter: 0.5,
         };
     }
 
@@ -22,29 +29,70 @@ class App extends Component {
     }
 
     loadScoreFromBackend(){
-
+        fetch("http://localhost:5000/")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    console.log(result);
+                    this.setState({
+                        instruments: result.instruments,
+                        tempo: result.tempo,
+                        midi_to_play: result.score
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
     }
 
-    playTestInstrument() {
+    stop(){
+        this.midiSounds.cancelQueue();
+        this.setState({currentOffset: 0});
+    }
+
+    downloadMidi(){
+
+    }
+    pause(){
+        this.midiSounds.cancelQueue();
+        this.setState({
+            currentOffset: (this.midiSounds.audioContext.currentTime
+                - this.state.startTime)/this.state.durationQuarter})
+    }
+
+    play() {
         // Request backend return json
         // If the user want he can download the midi file from the backend
-        let track_context = this.state.midi_to_play[i];
-        for(var i=0; i<this.props.state; i++){
-            let to_play = this.state.midi_to_play[i];
-            this.midiSounds.playChordAt(to_play.time, this.state.instruments[to_play.track],
-                to_play.pitch, to_play.duration);
+        this.midiSounds.setMasterVolume(0.3);
+        this.midiSounds.cancelQueue();
+        let bpm = this.state.tempo;
+        let N = 4 * 60 / bpm;
+        let durationQuarter = N/4;
+        let startTime = this.midiSounds.audioContext.currentTime;
+        this.setState({startTime: startTime, durationQuarter: durationQuarter})
+        for(let i=0; i < this.state.midi_to_play.length; i++){
+            let toPlay = this.state.midi_to_play[i];
+            if(toPlay.offset >= this.state.currentOffset){
+                let currentOffset = this.state.currentOffset;
+                this.midiSounds.playChordAt(startTime + (toPlay.offset - currentOffset)* durationQuarter,
+                    this.state.instruments[toPlay.track],
+                    [toPlay.pitch], toPlay.duration, toPlay.volume);
+            }
         }
     }
 
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to midi-sounds-react example 1</h1>
-                </header>
-                <p className="App-intro">Press Play to play instrument sound.</p>
-                <p><button onClick={this.playTestInstrument.bind(this)}>Play</button></p>
+                <p><button onClick={this.loadScoreFromBackend.bind(this)}>Load</button></p>
+                <p><button onClick={this.play.bind(this)}>Play</button></p>
+                <p><button onClick={this.pause.bind(this)}>Pause</button></p>
+                <p><button onClick={this.stop.bind(this)}>Stop</button></p>
+                <p><button onClick={this.downloadMidi.bind(this)}>Download midi</button></p>
                 <MIDISounds ref={(ref) => (this.midiSounds = ref)} appElementName="root" instruments={[3]} />
             </div>
         );
